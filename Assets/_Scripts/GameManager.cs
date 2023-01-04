7933e1c2736329b7f4374b757d7afdb3ca7d493d
paulm <paulmurfett@hotmail.com>
2023-01-04 13:03:28 +1000

    up to page 133

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+    private static GameManager _instance;
 
-    void Start() {
-        CameraSetup();
-        LightSetup();
+    public static GameManager Instance => _instance;
+    public static int CurrentScene = 0;
+    public static int GameLevelScene = 3;
+
+    private bool _died = false;
+
+    public bool Died {
+        get { return _died; }
+        set { _died = value; }
+    }
+
+    private void Awake() {
+        CheckGameManIsInScene();
+        CurrentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        LightAndCameraSetup(CurrentScene);
+    }
+
+    private void LightAndCameraSetup(int sceneNumber) {
+        switch (sceneNumber) {
+            case 3:
+            case 4:
+            case 5:
+            case 6: {
+                LightSetup();
+                CameraSetup();
+                break;
+            }
+        }
     }
 
     void CameraSetup() {
         GameObject gameCamera = GameObject.FindGameObjectWithTag("MainCamera");
-        
+
         gameCamera.transform.position = new Vector3(0, 0, -300);
         gameCamera.transform.eulerAngles = new Vector3(0, 0, 0);
 
@@ -26,8 +52,20 @@ public class GameManager : MonoBehaviour
         dirLight.GetComponent<Light>().color = new Color32(152, 204, 255, 255);
     }
 
-    void Update()
-    {
-        
+    private void CheckGameManIsInScene() {
+        if (_instance == null) {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start() {
+    }
+
+
+    void Update() {
     }
 }
