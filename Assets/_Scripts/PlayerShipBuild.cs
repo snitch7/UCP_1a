ba6619bb34da9995ecef30d48723079af6ba419f
paulm <paulmurfett@hotmail.com>
2023-01-07 13:09:49 +1000

    pg 249 - end of chapter

--- Assets/_Scripts/PlayerShipBuild.cs
+++ Assets/_Scripts/PlayerShipBuild.cs
@@ -1,13 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerShipBuild : MonoBehaviour {
     [SerializeField] private GameObject[] _shopButtons;
     private GameObject _target;
     GameObject _tempSelection;
+    private GameObject _textBoxPanel;
 
     private void Start() {
         TurnOffSelectionHighlights();
+        _textBoxPanel = GameObject.Find("textBoxPanel");
     }
 
     private void TurnOffSelectionHighlights() {
@@ -21,10 +24,32 @@ public class PlayerShipBuild : MonoBehaviour {
     }
 
     private void AttemptSelection() {
-        throw new NotImplementedException();
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hitInfo;
+            _target = ReturnClickedObject(out hitInfo);
+            if (_target!=null) {
+                if (_target.transform.Find("itemText")) {
+                    TurnOffSelectionHighlights();
+                    Select();
+                    UpdateDescriptionBox();
+                }
+            }
+        }
     }
 
-    GameObject ReturnClickedObject(RaycastHit hit) {
+    private void UpdateDescriptionBox() {
+        _textBoxPanel.transform.Find("name").gameObject.GetComponent<TextMesh>().text =
+            _tempSelection.GetComponentInParent<ShopPiece>().ShopSelection.IconName;
+        _textBoxPanel.transform.Find("desc").gameObject.GetComponent<TextMesh>().text =
+            _tempSelection.GetComponentInParent<ShopPiece>().ShopSelection.Description;
+    }
+
+    private void Select() {
+        _tempSelection = _target.transform.Find("SelectionQuad").gameObject;
+        _tempSelection.SetActive(true);
+    }
+
+    GameObject ReturnClickedObject(out RaycastHit hit) {
         GameObject target = null;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray.origin, ray.direction * 100, out hit)) {
--- Assets/_Scripts/ShopPiece.cs
+++ Assets/_Scripts/ShopPiece.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class ShopPiece : MonoBehaviour {
     [SerializeField] private SOShopSelection _shopSelection;
 
-    public SOShopSelection ShopSelection1 {
+    public SOShopSelection ShopSelection {
         get => _shopSelection;
         set => _shopSelection = value;
     }
@@ -13,6 +13,7 @@ public class ShopPiece : MonoBehaviour {
         // icon slot
         if (GetComponentInChildren<SpriteRenderer>() !=null) {
             GetComponentInChildren<TextMesh>().text = _shopSelection.Cost;
+            GetComponentInChildren<SpriteRenderer>().sprite = _shopSelection.Icon;
         }
     }
 }
