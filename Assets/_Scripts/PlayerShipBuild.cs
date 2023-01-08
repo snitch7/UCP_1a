f1a2d1fbaa1bf39c041c885991006616235831f6
snitch7 <paulmurfett@hotmail.com>
2023-01-08 16:13:43 +1000

    pg 260

--- Assets/_Scripts/PlayerShipBuild.cs
+++ Assets/_Scripts/PlayerShipBuild.cs
@@ -4,6 +4,16 @@ using UnityEngine.UI;
 
 public class PlayerShipBuild : MonoBehaviour {
     [SerializeField] private GameObject[] _shopButtons;
+
+    [SerializeField] GameObject[] _visualWeapons;
+    [SerializeField] private SOActorModel _defaultPlayerShip;
+    
+    private GameObject _playerShip;
+    private GameObject _buyButton;
+    private GameObject _bankObject;
+    private int _bank = 600;
+    private bool _purchaseMade = false;
+
     private GameObject _target;
     GameObject _tempSelection;
     private GameObject _textBoxPanel;
@@ -11,6 +21,23 @@ public class PlayerShipBuild : MonoBehaviour {
     private void Start() {
         TurnOffSelectionHighlights();
         _textBoxPanel = GameObject.Find("textBoxPanel");
+        _purchaseMade = false;
+        _bankObject = GameObject.Find("bank");
+        _bankObject.GetComponentInChildren<TextMesh>().text = _bank.ToString();
+        _buyButton = _textBoxPanel.transform.Find("BUY ?").gameObject;
+
+        TurnOffPlayerShipVisuals();
+        PreparePlayerShipForUpgrade();
+    }
+
+    private void PreparePlayerShipForUpgrade() {
+        throw new NotImplementedException();
+    }
+
+    private void TurnOffPlayerShipVisuals() {
+        for (int i = 0; i < _visualWeapons.Length; i++) {
+            _visualWeapons[i].gameObject.SetActive(false);
+        }
     }
 
     private void TurnOffSelectionHighlights() {
@@ -27,13 +54,44 @@ public class PlayerShipBuild : MonoBehaviour {
         if (Input.GetMouseButtonDown(0)) {
             RaycastHit hitInfo;
             _target = ReturnClickedObject(out hitInfo);
-            if (_target!=null) {
-                if (_target.transform.Find("itemText")) {
+            string itemTextCached = _target.transform.Find("itemText").GetComponent<TextMesh>().text;
+            Transform itemTransformCached = _target.transform.Find("itemText");
+            if (_target != null) {
+                if (itemTransformCached) {
                     TurnOffSelectionHighlights();
                     Select();
                     UpdateDescriptionBox();
+
+                    // if not already sold:
+                    if (itemTextCached != "SOLD") {
+                        Affordable();
+                        LackOfFunds();
+                    }
+                    else if (itemTextCached == "SOLD") {
+                        SoldOut();
+                    }
                 }
             }
+        }
+    }
+
+    private void SoldOut() {
+        print("Sold Out!");
+    }
+
+    private void LackOfFunds() {
+        int itemCost = System.Int32.Parse(_target.transform.GetComponent<ShopPiece>().ShopSelection.Cost);
+        if (_bank < itemCost) {
+            print("Cannot buy");
+            _buyButton.SetActive(false);
+        }
+    }
+
+    private void Affordable() {
+        int itemCost = System.Int32.Parse(_target.transform.GetComponent<ShopPiece>().ShopSelection.Cost);
+        if (_bank >= itemCost) {
+            print("Can buy");
+            _buyButton.SetActive(true);
         }
     }
 
